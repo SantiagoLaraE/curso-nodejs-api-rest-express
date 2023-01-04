ce1293669993a4102a4025f0ba1833cb4822a94d
SantiagoLaraE <oscarescobar773@gmail.com>
2023-01-03 21:42:56 -0500

    Router general para versionamiento y obtener información mediante petición POST

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
+const express = require('express');
+
 const productsRouter = require('./products.router.js');
 const usersRouter = require('./users.router.js');
 const categoriesRouter = require('./categories.router.js');
 
 function routerApi(app) {
-  app.use('/api/products', productsRouter);
-  app.use('/api/users', usersRouter);
-  app.use('/api/categories', categoriesRouter);
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter);
+  router.use('/users', usersRouter);
+  router.use('/categories', categoriesRouter);
 }
 
 module.exports = routerApi;
