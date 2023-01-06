5c55a6ec1373a335d370d7f5ed5f74eb13dc2908
SantiagoLaraE <oscarescobar773@gmail.com>
2023-01-05 21:59:05 -0500

    Ajuste CORS

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
-const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler')
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+// const whitelist = [''];
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Acceso no permitido'));
+//     }
+//   },
+// };
+app.use(cors());
 
 routerApi(app);
 
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
-
 
 app.listen(port, () => {
   console.log(`Corriendo en: ${port}`);
