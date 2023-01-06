3b7e61d34ce83a74cbf4dc5d24db19f4d01074c9
SantiagoLaraE <oscarescobar773@gmail.com>
2023-01-05 22:35:17 -0500

    Ajuste de puerto

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {
   boomErrorHandler,
 } = require('./middlewares/error.handler');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 // const whitelist = [''];
